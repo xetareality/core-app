bee4ebbc4ac3b436274ea18034ff8bb9246bc3ef
xetadev <dev@xetareality.com>
2021-11-27 11:55:17 +0200

    feat: refactor

--- dist/scripts/utils.js
+++ dist/scripts/utils.js
@@ -1,14 +1,14 @@
 function debounce(func, wait, immediate) {
-    var timeout;
+    var timedata;
     return function() {
         var context = this, args = arguments;
         var later = function() {
-            timeout = null;
+            timedata = null;
             if (!immediate) func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+        var callNow = immediate && !timedata;
+        clearTimedata(timedata);
+        timedata = setTimedata(later, wait);
         if (callNow) func.apply(context, args);
     };
 };
@@ -21,6 +21,36 @@ function strip(obj) {
     return out
 }
 
+/**
+ * Wrap resource
+ */
+var wrap = {
+    transaction: function(signature) {
+        return '<a href="/transaction/?signature='+signature+'" class="underline hover:text-pink-400">'+signature+'</a>'
+    },
+    token: function(address, tokenPreview) {
+        var preview = tokenPreview ? '<span class="ml-2">'+tokenPreview+'</span>' : ''
+        return '<a href="/token/?address='+address+'" class="underline hover:text-pink-400">'+address+'</a>'+preview
+    },
+    pool: function(address, poolPreview) {
+        var preview = poolPreview ? '<span class="ml-2">'+poolPreview+'</span>' : ''
+        return '<a href="/pool/?address='+address+'" class="underline hover:text-pink-400">'+address+'</a>'+preview
+    },
+    address: function(address) {
+        return '<a href="/address/?address='+address+'" class="underline hover:text-pink-400">'+address+'</a>'
+    },
+    claim: function(hash) {
+        return '<a href="/claim/?hash='+hash+'" class="underline hover:text-pink-400">'+hash+'</a>'
+    },
+    allowance: function(hash) {
+        return '<a href="/allowance/?hash='+hash+'" class="underline hover:text-pink-400">'+hash+'</a>'
+    },
+    link: function(link) {
+        return '<a rel="nofollow" href="'+link+'" class="underline hover:text-pink-400 w-full block truncate pb-1">'+link+'</a>'
+    }
+}
+
+
 function showModal(name) {
     document.body.classList.add('overflow-hidden')
     Alpine.store('modal', name)
@@ -31,18 +61,36 @@ function hideModal() {
     Alpine.store('modal', null)
 }
 
-function refreshAll() {
-    Array.prototype.slice.call(document.querySelectorAll('*[x-data]')).filter(function (el) {return !!el.__x})
-    .forEach(function (el) {el.__x.updateElements(el)});
-}
-
-function refreshElement(el) {
-    el.__x.updateElements(el);
+function setData(id, data) {
+    for (key in data) {
+        document.getElementById(id)._x_dataStack[0][key] = data[key]
+    }
 }
 
 function ajax(url, data, success, error) {try {x = new(this.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');x.open(data ? 'POST' : 'GET', url, 1);x.onload = function (e) {success && success(this.responseText, this.status)};x.onerror = function (e) {error && error(this.responseText, this.status)};x.send(JSON.stringify(data))} catch (e) {window.console && console.log(e)}};
 function gup(e,l){l||(l=location.href),e=e.replace(/[\\[]/,"\\\\[").replace(/[\\]]/,"\\\\]");var n=new RegExp("[\\\\?&]"+e+"=([^&#]*)").exec(l);return null==n?null:n[1]};
 
+/**
+ * Set page meta (title, description, etc)
+ */
+function setPageMeta(resource, data) {
+    var desc = ''
+    if (resource == 'transaction') {
+        document.title = document.title+' '+data.signature
+        desc = 'Transaction '+data.signature+' was registered on '+new Date(parseInt(data.created)).toLocaleString()
+    } else if (resource == 'token') {
+        document.title = document.title+(data.name ? ' '+data.name : '')+(data.ticker ? ' '+data.ticker : '')
+        desc = (data.name ? data.name : data.address)+(data.ticker ? ' '+data.ticker : '')+' was registered on '+new Date(parseInt(data.created)).toLocaleString()+' and has a supply of '+data.supply.replace('-', '')
+    } else if (resource == 'pool') {
+        document.title = document.title+' '+(data.name ? ' '+data.name : '')+' ('+data.program[0].toUpperCase()+data.program.slice(1)+'-Pool)'
+        desc = (data.name ? data.name : data.address)+' is a '+data.program[0].toUpperCase()+data.program.slice(1)+'-Pool registered on '+new Date(parseInt(data.created)).toLocaleString()
+    } else if (resource == 'address') {
+        document.title = document.title+' '+data.address
+        desc = data.address+' is a public address on the Xeta blockchain'
+    }
+
+    document.querySelector('meta[name="description"]').setAttribute('content', desc)
+}
 
 /**
  * Validation
@@ -73,7 +121,7 @@ function validForm(form) {
     var elements = form.elements;
     for (var i = 0; i < elements.length; i++) {
         var item = elements[i];
-        if (item.name) {
+        if (['INPUT', 'TEXTAREA'].includes(item.tagName)) {
             var v = validate(item)
             if (!v) return;
         }
@@ -81,37 +129,65 @@ function validForm(form) {
     return true
 }
 
-function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
+function formatNumber(val, decimals=0) {
+    return parseFloat(val).toLocaleString('en', {maximumFractionDigits: decimals})
+}
+
+function formatCurrency(val, decimals=0) {
+    return parseFloat(val).toLocaleString('en', {style: 'currency', currency: 'USD', maximumFractionDigits: decimals})
+}
+
+function formatTime(date, prep=true) {
+    var future = (Date.now() - parseInt(date)) < 0
+    var seconds = Math.abs(Math.floor((Date.now() - date) / 1000))
     var interval = seconds / 31536000;
     var i = null
+    var format = function(val) {
+        if (!prep) return val
+        return (future ? 'In ' : '')+val+(!future ? ' ago' : '')
+    }
 
     if (interval > 1) {
         i = Math.floor(interval)
-        return i + ' year'+(i > 1 ? 's' : '');
+        return format(i + ' year'+(i > 1 ? 's' : ''))
     }
     interval = seconds / 2592000;
     if (interval > 1) {
         i = Math.floor(interval)
-        return i + ' month'+(i > 1 ? 's' : '');
+        return format(i + ' month'+(i > 1 ? 's' : ''))
     }
     interval = seconds / 86400;
     if (interval > 1) {
         i = Math.floor(interval)
-        return i + ' day'+(i > 1 ? 's' : '');
+        return format(i + ' day'+(i > 1 ? 's' : ''))
     }
     interval = seconds / 3600;
     if (interval > 1) {
         i = Math.floor(interval)
-        return i + ' hour'+(i > 1 ? 's' : '');
+        return format(i + ' hour'+(i > 1 ? 's' : ''))
     }
     interval = seconds / 60;
     if (interval > 1) {
         i = Math.floor(interval)
-        return i + ' minute'+(i > 1 ? 's' : '');
+        return format(i + ' minute'+(i > 1 ? 's' : ''))
     }
     i = Math.floor(seconds)
-    return i + ' second'+(i != 1 ? 's' : '');
+    return format(i + ' second'+(i != 1 ? 's' : ''))
+}
+
+function timer(expires) {
+    var remaining =  parseInt((expires - Date.now()) / 1000)
+    if (remaining <= 0) return 'Expired on '+(new Date(parseInt(expires)).toLocaleString())
+
+    var days = parseInt(remaining / 86400)
+    var daysRemanining = remaining % 86400
+    var hours = parseInt(daysRemanining / 3600)
+    var hoursRemaining = daysRemanining % 3600
+    var minutes = parseInt(hoursRemaining / 60)
+    var minutesRemaining = hoursRemaining % 60
+    var seconds = parseInt(minutesRemaining)
+
+    return days+' Days, '+hours+' Hours, '+minutes+' Minutes, '+seconds+' Seconds'
 }
 
 function currentPeriod() {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,13 @@
 document.addEventListener('alpine:init', () => {
+    // Init
+    Alpine.store('pool', {})
+    Alpine.store('transaction', {})
+    Alpine.store('token', {})
+    Alpine.store('address', {})
+    Alpine.store('allowance', {})
+    Alpine.store('claim', {})
+    Alpine.store('balances', [])
+
     // Session store
     Alpine.sstore = function(name, value) {
         if (value != null && value != undefined) {
@@ -29,116 +38,45 @@ document.addEventListener('alpine:init', () => {
     Alpine.magic('lstore', () => {return (name, value) => Alpine.lstore(name, value)})
 
     if (Alpine.lstore('publicKey')) connectWallet(Alpine.lstore('publicKey'))
+
+    // Load resource
+    var resource = window.location.pathname.slice(1, -1)
+    if (['pool', 'address', 'transaction', 'token', 'claim', 'allowance'].includes(resource)) {
+        var key = resource == 'transaction' ? gup('signature') : gup('address')
+        Xeta[resource].get({[resource == 'transaction' ? 'signature' : 'address']: key, extended: true}).then(function(data) {
+            Alpine.store(resource, data)
+            if (resource == 'pool') Alpine.store(data.program, new Xeta[data.program](data))
+            setPageMeta(resource, data)
+
+            var event = document.createEvent('Event')
+            event.initEvent('resourceLoaded', false, false)
+            document.dispatchEvent(event)
+        })
+    }
 })
 
 
-const INTERFACE = 'https://interface.xetareality.com'
-const NETWORK = 'https://testnet.xetareality.com'
-
-const Constants = {
-    zeroAddress: '11111111111111111111111111111zero',
-    xetaAddress: '11111111111111111111111111111xeta',
-    factoryAddress: '11111111111111111111111111factory',
-    xusdAddress: '11111111111111111111111111111xusd',
-    sponsoredAddress: '1111111111111111111111111sponsored',
-    consumedAddress: '11111111111111111111111111consumed',
-    batchAddress: '1111111111111111111111111111batch',
-}
-
-function connectWallet(publicKey) {
-    Alpine.lstore('publicKey', publicKey)
-    refreshBalances(publicKey)
+function connectWallet() {
+    Xeta.wallet.connect({
+        publicKey: Alpine.sstore('publicKey'),
+        privateKey: Alpine.sstore('privateKey'),
+        seed: Alpine.sstore('seed'),
+        password: Alpine.sstore('password'),
+        networkEndpoint: 'http://127.0.0.1:3000',
+        interfaceEndpoint: 'http://127.0.0.1:8787'})
+    
+    refreshBalances()
 }
 
 function disconnectWallet() {
-    Alpine.lstore('publicKey', '')
+    Alpine.sstore('publicKey', '')
+    Alpine.sstore('privateKey', '')
+    Alpine.sstore('seed', '')
+    Alpine.sstore('password', '')
 }
 
-function refreshBalances(publicKey) {
-    ajax(API+'/balances?address='+publicKey, null, function(data) {
-        Alpine.lstore('balances', JSON.parse(data))
-    })
-}
-
-const MAPPING = {
-    transaction: {
-        signature: ['transaction', 1],
-        from: ['address', 1],
-        to: ['address', 1],
-        sender: ['address', 1],
-        token: ['token', 1],
-        amount: ['number', 1],
-        fee: ['number', 1],
-        nonce: ['number', 1],
-        created: ['timestamp', 1],
-        message: ['text', 1],
-        function: ['string', 1],
-        delegate: ['boolean', 1],
-        error: ['string', 1],
-        confirmed: ['timestamp', 1],
-        input: ['transaction', 1],
-        outputs: ['transactions', 1],
-        confirmed: ['timestamp', 1],
-        confirmations: ['number', 1],
-        payerBalance: ['number', 1],
-        fromBalance: ['number', 1],
-        toBalance: ['number', 1],
-    },
-    token: {
-        address: ['string', 1],
-        creator: ['address', 1],
-        name: ['string', 1],
-        ticker: ['string', 1],
-        supply: ['number', 1],
-        reserve: ['number', 1],
-        created: ['timestamp', 1],
-
-        description: ['string', 1],
-        links: ['links', 1],
-        icon: ['string', 0],
-        object: ['string', 0],
-
-        mime: ['string', 0],
-        meta: ['string', 0],
-        hash: ['string', 0],
-        fingerprint: ['string', 0],
-        cluster: ['string', 0],
-    },
-    pool: {
-        address: ['pool', 1],
-        creator: ['address', 1],
-        token: ['token', 1],
-        program: ['string', 1],
-        created: ['timestamp', 1],
-
-        name: ['string', 1],
-        mechanism: ['string', 1],
-        candidates: ['strings', 0],
-        rate: ['number', 1],
-        percentage: ['number', 1],
-        probability: ['number', 1],
-        answers: ['strings', 0],
-
-        expires: ['timestamp', 1],
-        minAmount: ['number', 1],
-        maxAmount: ['number', 1],
-        minTime: ['timedelta', 1],
-        maxTime: ['timedelta', 1],
-        transfersLimit: ['number', 1],
-        claimsLimit: ['number', 1],
-        tokenLimit: ['number', 1],
-        xetaLimit: ['number', 1],
-        tokenTarget: ['number', 1],
-        xetaTarget: ['number', 1],
-
-        xetaBalance: ['number', 1],
-        tokenBalance: ['number', 1],
-        xetaTurnover: ['number', 1],
-        tokenTurnover: ['number', 1],
-        transfersCount: ['number', 1],
-        claimsCount: ['number', 1],
-        closed: ['boolean', 1],
-        leader: ['address', 1],
-        meta: ['string', 1]
-    }
+function refreshBalances() {
+    Xeta.balance.scanByAddress({address: Alpine.sstore('publicKey')}).then(function(data) {
+        Alpine.sstore('balances', data)
+    }).catch(function(e) {alert(e)})
 }
